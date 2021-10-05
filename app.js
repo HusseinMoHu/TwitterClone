2003b74686b68ba11b3e57e1cd7375e6f89bbe36
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-10-05 19:39:43 +0200

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const middleware = require("./middleware");
 const path = require("path");
 const mongoose = require("./database");
@@ -22,7 +23,7 @@ const io = require("socket.io")(server, {
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Start session
